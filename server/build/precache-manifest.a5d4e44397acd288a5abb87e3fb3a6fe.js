dd3dd436fb53e9504015b222d1617862520397d4
madeel42 <madeelakhtar204@gmail.com>
2020-04-08 15:11:04 -0700

    'chatapp'

---
+++ server/build/precache-manifest.a5d4e44397acd288a5abb87e3fb3a6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a1b627c79b75cff60a214dc950af0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7caa6c36ca12fb97df0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fff992684ea9a02c711f",
+    "url": "/static/js/2.e5dcad90.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.e5dcad90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7caa6c36ca12fb97df0",
+    "url": "/static/js/main.ab3d9c83.chunk.js"
+  },
+  {
+    "revision": "d3de520cb16c5948bdda",
+    "url": "/static/js/runtime-main.e9580a83.js"
+  }
+]);
